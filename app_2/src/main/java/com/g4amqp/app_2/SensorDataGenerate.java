2bc62933b7043117ad428b3577b8235f9e230bdd
kienlt <kinlng2002@gmail.com>
2023-05-05 22:44:16 +0700

    app2 ver4

--- app_2/src/main/java/com/g4amqp/app_2/SensorDataGenerate.java
+++ app_2/src/main/java/com/g4amqp/app_2/SensorDataGenerate.java
@@ -1,11 +1,13 @@
 package com.g4amqp.app_2;
 import java.io.IOException;
+import java.sql.Date;
 import java.util.Random;
 import java.util.concurrent.TimeoutException;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.Connection;
-// import com.google.gson.Gson;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class SensorDataGenerate {
     private static final String QUEUE_NAME = "sensor_data_queue";
@@ -28,18 +30,26 @@ public class SensorDataGenerate {
         
         Random random = new Random();
         Sensor[] sensors = new Sensor[NUMBER_OF_SENSORS];
-       
+
         for (int i = 0; i < NUMBER_OF_SENSORS; i++) {
             sensors[i] = new Sensor(i + 1, "Sensor " + (i + 1), 0, 0);
         }
 
         while (true) {
             for (Sensor sensor : sensors) {
+                LocalTime now = LocalTime.now();
+                int hour = now.getHour();
+                int minute = now.getMinute();
+                int second = now.getSecond();
                 int temperature = random.nextInt(MAX_TEMPERATURE);
                 int humidity = random.nextInt(MAX_HUMIDITY);
+                
                 sensor.setTemperature(temperature);
                 sensor.setHumidity(humidity);
-                String message = "{\"temperature\": " + temperature + ", \"humidity\": " + humidity + "}";
+
+                String message = "{\"time\": " + hour + ":" + minute + ":" + second +
+                ", \"temperature\": " + temperature + 
+                ", \"humidity\": " + humidity + "}";
                 channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
                 System.out.println(message);
             }
