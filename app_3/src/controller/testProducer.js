e1db6df7f08a0cff474867dfc61680c9409c73c4
tuananh0102 <anhtt0102@gmail.com>
2023-05-08 20:23:25 +0700

    update testProducer

--- app_3/src/controller/testProducer.js
+++ app_3/src/controller/testProducer.js
@@ -5,7 +5,7 @@ async function connect() {
       "amqp://g4amqp:g4amqp@g4amqp.freeddns.org:5672"
     );
     const channel = await connection.createChannel();
-    await channel.assertQueue("temperature");
+    // await channel.assertQueue("sensor_data_queue");
     setInterval(async () => {
       const now = new Date();
       const hoursAndMinutes =
@@ -13,10 +13,11 @@ async function connect() {
       const msgBuffer = Buffer.from(
         JSON.stringify({
           time: hoursAndMinutes,
-          temperature: Math.floor(Math.random() * 100),
+          temperature: Math.floor(Math.random() * 50),
+          humidity: Math.floor(Math.random() * 100),
         })
       );
-      await channel.sendToQueue("temperature", msgBuffer);
+      await channel.sendToQueue("sensor_data_queue", msgBuffer);
       console.log("Sending message to temperature queue");
     }, 2000);
   } catch (ex) {
